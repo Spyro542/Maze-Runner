aea9da87993b7ddfc8a86b5a3b211bc1c23c94bc
Spyro542 <thomasandersen542@gmail.com>
2026-03-09 08:33:48 +0000

    Worked with delta time

--- Maze Runner/Maze Runner.cpp
+++ Maze Runner/Maze Runner.cpp
@@ -1,4 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include <chrono>
+
+
 
 #include "Input.h"
 #include "Screen.h"
@@ -12,37 +16,53 @@ int main()
 
 	while (true)
 	{
+		auto Start_Time = std::chrono::system_clock::now();
 		Clear_Inputs();
 		//while (Get_Input() > 0){}	//Stop Repeated keystrokes
 		//while (!(Get_Input() > 0))
-		{
-			switch (Get_Input()) {
-			case Up:
-				std::cout << "Up\n";
-				break;
-			case Down:
-				std::cout << "Down\n";
-				break;
-			case Left:
-				std::cout << "Left\n";
-				break;
-			case Right:
-				std::cout << "Right\n";
-				break;
-			case Enter:
-				std::cout << "Enter\n";
-				break;
-			case Esc:
-				std::cout << "Esc\n";
-				break;
-			case Return:
-				std::cout << "Return\n";
-				break;
-			case Interact:
-				std::cout << "Interact\n";
-				break;
-			}
+		
+		switch (Get_Input()) {
+		case Up:
+			std::cout << "Up\n";
+			break;
+		case Down:
+			std::cout << "Down\n";
+			break;
+		case Left:
+			std::cout << "Left\n";
+			break;
+		case Right:
+			std::cout << "Right\n";
+			break;
+		case Enter:
+			std::cout << "Enter\n";
+			break;
+		case Esc:
+			std::cout << "Esc\n";
+			break;
+		case Return:
+			std::cout << "Return\n";
+			break;
+		case Interact:
+			std::cout << "Interact\n";
+			break;
+		
 		}
+		for (size_t i = 0; i < 16; i++)
+		{
+			for (size_t i = 0; i < 16; i++)
+			{
+				std::cout << "#";
+			}
+			std::cout << "\n";
+		}
+		auto Current_Time = std::chrono::system_clock::now();
+		std::chrono::duration<double> Delta_Time = Current_Time - Start_Time;
+		
+
+		std::cout << "elapsed time: " << Delta_Time.count() << "s"
+			<< std::endl;
+		system("cls");
 	}
 
 }
